d60b7257f8fd5a897bb9ee7e535f92e69f1fa3fd
turkbey <turkbey.yozcu@gmail.com>
2025-04-20 17:08:26 +0300

    .

---
+++ src/main/java/com/turkbey/Main.java
@@ -0,0 +1,7 @@
+package com.turkbey;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This homework is for Software Validation and Verification!");
+    }
+}
